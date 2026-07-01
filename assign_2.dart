3bf7af21bcb236fc88cab8dd0147e321eed7fcf8
sharif-ahmed-dev <mail.sharif.dev@gmail.com>
2026-07-02 01:07:24 +0600

    Asssignment of Module - 2

---
+++ assign_2.dart
@@ -0,0 +1,34 @@
+void main() {
+  bool isNewStudent = true;
+  List<String> studentNames1 = ["Minhaz", "Jillur", "Sabbir"];
+  List<String> studentNames2 = [
+    "Sharif",
+    "Matin",
+    if (isNewStudent) "Rahim"];
+  List<String> studentNames = [...studentNames1, ...studentNames2];
+
+  Set<String> enrolledCourses = {"Flutter", "Dart", "Git", "C"};
+
+//amr and batchmate der name and age
+  Map<String, dynamic> studentAges = {
+    "Sharif": 21,
+    "Matin": 22,
+    
+    "Rahim": 25,
+    "Minhaz": 20,
+    "Jillur": 23,
+    "Sabbir": 24
+  };
+
+  print("Students:\n$studentNames\n");
+  print("Courses:\n$enrolledCourses\n");
+  print("Student Ages:");
+  for (var student in studentAges.entries)
+  //var diye type inferred korsi, then variable er nam disi
+  {
+    print("${student.key} -> ${student.value}"); 
+    //student.key diye name ar student.value diye age print korsi
+    //loop er moto cholbe jotokkhon porjonto entry ase
+  }
+
+}
