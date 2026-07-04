710a1902e856d1c0b8965c4e47461b4531d244ff
Sharif Ahmed <mail.sharif.dev@gmail.com>
2026-07-05 01:34:30 +0600

    Fixed code format

--- assignment_2.dart
+++ assignment_2.dart
@@ -1,23 +1,20 @@
 void main() {
   bool isNewStudent = true;
   List<String> studentNames1 = ["Minhaz", "Jillur", "Sabbir"];
-  List<String> studentNames2 = [
-    "Sharif",
-    "Matin",
-    if (isNewStudent) "Rahim"];
+  List<String> studentNames2 = ["Sharif", "Matin", if (isNewStudent) "Rahim"];
   List<String> studentNames = [...studentNames1, ...studentNames2];
 
   Set<String> enrolledCourses = {"Flutter", "Dart", "Git", "C"};
 
-//amr and batchmate der name and age
+  //amr and batchmate der name and age
   Map<String, dynamic> studentAges = {
     "Sharif": 21,
     "Matin": 22,
-    
+
     "Rahim": 25,
     "Minhaz": 20,
     "Jillur": 23,
-    "Sabbir": 24
+    "Sabbir": 24,
   };
 
   print("Students:\n$studentNames\n");
@@ -26,9 +23,8 @@ void main() {
   for (var student in studentAges.entries)
   //var diye type inferred korsi, then variable er nam disi
   {
-    print("${student.key} -> ${student.value}"); 
+    print("${student.key} -> ${student.value}");
     //student.key diye name ar student.value diye age print korsi
     //loop er moto cholbe jotokkhon porjonto entry ase
   }
-
 }
